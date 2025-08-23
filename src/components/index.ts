0905f3faab5e28dcfdacb1e582ee9c15f9f7f6d8
Gustavo Tavares <gustavotavares0212@gmail.com>
2025-08-23 12:52:50 -0300

    index ts de components ate molecules

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { Button } from './atoms/Button';
+export { Input } from './atoms/Input';
+export { Label } from './atoms/Label';
+export { Icon } from './atoms/Icon';
+export { Text } from './atoms/Text/Index';
+
+export { FormField } from './molecules/FormField';
+export { Card } from './molecules/Card';
+export { SearchBox } from './molecules/SearchBox/Index';
+export { NavigationItem } from './molecules/NavigationItem/index';
